8eacb0abd53930f51c0594b2b56d4ce83afaf7a8
rooooonald <rooonaldjp@gmail.com>
2024-04-02 20:57:06 -0400

    Adding placeholder for index images

--- src/components/home/CountryCardStack.jsx
+++ src/components/home/CountryCardStack.jsx
@@ -1,8 +1,17 @@
-import styles from "./CountryCardStack.module.scss";
+import { useEffect, useState } from "react";
 
+import Loader from "react-js-loader";
+
+import styles from "./CountryCardStack.module.scss";
 import { LazyMotion, domAnimation, m } from "framer-motion";
 
 export default function CountryCardStack({ activeCountry, countryName }) {
+  const [isImgLoading, setIsImgLoading] = useState(true);
+
+  useEffect(() => {
+    setIsImgLoading(true);
+  }, [activeCountry]);
+
   return (
     <LazyMotion features={domAnimation}>
       <div className={styles["country-display"]}>
@@ -11,17 +20,27 @@ export default function CountryCardStack({ activeCountry, countryName }) {
           initial={{ rotate: 30 }}
           animate={{ rotate: 15 }}
           transition={{ delay: 0.1 }}
+          className={styles["country-card"]}
         >
+          {isImgLoading && (
+            <div className={styles.placeholder}>
+              <img src="/images/home/stamps.webp" />
+              <div className={styles["loader-container"]}>
+                <Loader type="bubble-loop" size={50} />
+              </div>
+            </div>
+          )}
           <img
             src={`/images/countries/${activeCountry}.webp`}
             alt={countryName}
+            onLoad={() => setIsImgLoading(false)}
           />
           <p>{countryName.toUpperCase()}</p>
         </m.div>
-        <div>
+        <div className={styles["country-card"]}>
           <img src={"/images/countries/AO.webp"} />
         </div>
-        <div>
+        <div className={styles["country-card"]}>
           <img src={"/images/countries/SA.webp"} />
         </div>
       </div>
